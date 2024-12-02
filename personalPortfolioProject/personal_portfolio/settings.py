11852c009730c7be74c626453e3c4dda94ca024b
Brais <a24braisgp@iessanclemente.net>
2024-12-02 09:48:46 +0000

    a

--- personalPortfolioProject/personal_portfolio/settings.py
+++ personalPortfolioProject/personal_portfolio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8_mpy02#9th6id#*#j^=grv8-ek0_flu@dksa0#qt$kozjj@b('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['abrais24gp.pythonanywhere.com']
 
 
 # Application definition
@@ -117,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
